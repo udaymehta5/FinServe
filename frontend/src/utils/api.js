c4badf52019e0776036d5d04e2915dc4ab4f0d27
codester <uday.23b153188@abes.ac.in>
2026-06-28 19:16:40 +0530

    last commit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://your-render-url.onrender.com/api',
+  baseURL: 'https://finserve-backend-qqwf.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
